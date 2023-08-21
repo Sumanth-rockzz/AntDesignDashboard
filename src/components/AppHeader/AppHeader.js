c687735d7455fdc377a5416340bbce48b4d08476
Sumanth-rockzz <Sumanthn876@gmail.com>
2023-08-22 03:29:45 +0530

    some fixes in build folder

--- src/components/AppHeader/AppHeader.js
+++ src/components/AppHeader/AppHeader.js
@@ -22,15 +22,10 @@ const AppHeader = () => {
 
   return (
     <div className="AppHeader">
-      <Space>
-        <Image
-          width={40}
-          src="https://images.fastcompany.net/image/upload/w_1280,f_auto,q_auto,fl_lossy/w_596,c_limit,q_auto:best,f_auto/fc/3034007-inline-i-applelogo.jpg"
-        ></Image>
-        <Typography.Title level={4} className="BrandName">
-          <b>InstaMart</b>
-        </Typography.Title>
-      </Space>
+      <Image
+        width={40}
+        src="https://images.fastcompany.net/image/upload/w_1280,f_auto,q_auto,fl_lossy/w_596,c_limit,q_auto:best,f_auto/fc/3034007-inline-i-applelogo.jpg"
+      ></Image>
       <Typography.Title className="BrandName" level={4}>
         InstaMart DashBoard
       </Typography.Title>
